532e607e0bd1a31fa971f83bab000ec59bb6aa7f
sholev <asen.sholev@gmail.com>
2015-12-03 21:15:08 +0200

    Changes to the Level class

--- LeGame/Models/Levels/Level.cs
+++ LeGame/Models/Levels/Level.cs
@@ -19,7 +19,13 @@ namespace LeGame.Models.Levels
             }
             List<string> textFileRows = File.ReadAllLines(textFilePath).ToList();
             int separatorLocation = textFileRows.FindIndex(s => s.Contains("Legend:"));
+            if (separatorLocation == -1)
+            {
+                // TODO: Make a new exception for this case.
+                throw new Exception($"Map file \"{textFilePath}\" doesn't contain \"Legend:\" separator.");
+            }
 
+            // Split the map matrix and the legend rows.
             List<string> mapRows = textFileRows.Take(separatorLocation).ToList();
             IEnumerable<string> legendRows = textFileRows.Skip(separatorLocation + 1);
             Dictionary<char, string> legend = legendRows.ToDictionary(item => item[0], item => item.Substring(2));
@@ -30,20 +36,27 @@ namespace LeGame.Models.Levels
                 for (int col = 0; col < mapRows[row].Length; col++)
                 {
                     char currentChar = mapRows[row][col];
-                    if (legend.ContainsKey(currentChar))
+                    
+                    if (!legend.ContainsKey(currentChar))
                     {
-                        string[] parameters = legend[currentChar].Split(':');
-
-                        string textureFile = parameters[0];
-                        bool hasCollision = parameters[1].Equals("true");
-
-                        var texture = content.Load<Texture2D>(textureFile);
-                        var position = new Vector2(col * texture.Width, row * texture.Height);
-
-                        Tiles.Add(new Tile(position, texture, hasCollision));
+                        // TODO: Make a new exception for this case.
+                        throw new Exception($"Invalid legend in \"{textFilePath}\" file.");
                     }
+
+                    string[] parameters = legend[currentChar].Split(':');
+
+                    string textureFile = parameters[0];
+                    bool hasCollision = parameters[1].Equals("true");
+                    int drawPriority = parameters.Length > 2 ? int.Parse(parameters[2]) : 0;
+                    
+                    var texture = content.Load<Texture2D>(textureFile);
+                    var position = new Vector2(col * 32, row * 32);
+
+                    Tiles.Add(new Tile(position, texture, hasCollision, drawPriority));
                 }
             }
+            // Sort the list based on drawPriority, so it can be drawn in proper order if needed.
+            Tiles = Tiles.OrderBy(t => t.DrawPriority).ToList();
         }
 
         public List<Tile> Tiles { get; }
@@ -53,12 +66,14 @@ namespace LeGame.Models.Levels
             public Vector2 Position;
             public Texture2D Image;
             public bool HasCollision;
+            public int DrawPriority;
 
-            public Tile(Vector2 position, Texture2D image, bool hasCollision)
+            public Tile(Vector2 position, Texture2D image, bool hasCollision, int drawPriority)
             {
                 this.Position = position;
                 this.Image = image;
                 this.HasCollision = hasCollision;
+                this.DrawPriority = drawPriority;
             }
         }
     }
