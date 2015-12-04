b3641fd0eb92a6cbe265e18e540138c2025ad9a8
sholev <asen.sholev@gmail.com>
2015-12-04 21:33:59 +0200

    Separation of Assets and Tiles and the use of AssetBuilder

--- LeGame/Models/LevelAssets/Tile.cs
+++ LeGame/Models/LevelAssets/Tile.cs
@@ -9,42 +9,42 @@ using LeGame.Models;
 
 namespace LeGame.Models.LevelAssets
 {
-    public struct Tile : IGameObject, ICollisionable //ICollisionable should be removed
+    public struct Tile : IGameObject//, ICollisionable //ICollisionable should be removed
     {
         private Vector2 position;
         private string id;
         private string displayName;
-      
 
-
-        public Tile(Vector2 position, Texture2D image, bool canCollide, int drawPriority)
+        public Tile(Vector2 position, Texture2D texture, int drawPriority)
             : this()
         {
             this.Position = position;
-            this.Image = image;
+            this.Texture = texture;
             this.DrawPriority = drawPriority;
-            this.CanCollide = canCollide;
             this.Id = "Tile";
             this.DisplayName = "Nothing right now"; //maybe deserves a logical improvement?
         }
+
         public  Vector2 Position
         {
             get { return this.position; }
             set { this.position = value; }
         }
+
         public string Id
         {
             get { return this.id; }
             set { this.id = value; }
-
-
         }
-        public string DisplayName {
+
+        public string DisplayName
+        {
             get { return this.displayName; }
             set { this.displayName = value; }
         }
-        public Texture2D Image { get; set; }
+
+        public Texture2D Texture { get; set; }
         public int DrawPriority { get; set; }
-        public bool CanCollide { get; set; } // should be removed as well
+        //public bool CanCollide { get; set; } // should be removed as well
     }
 }
