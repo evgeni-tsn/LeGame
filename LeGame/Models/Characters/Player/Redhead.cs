e9c4b9efe66647ea0afd659441500652ebf70aa8
sholev <asen.sholev@gmail.com>
2015-12-15 14:51:36 +0200

    Third character added

---
+++ LeGame/Models/Characters/Player/Redhead.cs
@@ -0,0 +1,23 @@
+﻿namespace LeGame.Models.Characters.Player
+{
+    using Microsoft.Xna.Framework;
+
+    public class Redhead : Player
+    {
+        private const string RedheadDefaultType = "Player/p3Rotation";
+
+        private const int RedheadDefaultMaxHealth = 150;
+
+        private const int RedheadDefaultCurrentHealth = 150;
+
+        private const int RedheadDefaultSpeed = 2;
+
+        private const int RedheadDefaultHitCooldown = 2000;
+
+        public Redhead(Vector2 position, Level level)
+            : base(position, RedheadDefaultType, RedheadDefaultMaxHealth, RedheadDefaultCurrentHealth, RedheadDefaultSpeed, RedheadDefaultHitCooldown, level)
+        {
+            
+        }
+    }
+}
